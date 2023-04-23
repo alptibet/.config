b5e9d385b121b2b866fd1b7659290d8fbb0f2a75
alptibet <a.tibet@yandex.ru>
2023-04-23 21:09:17 +0300

    fixed neotree sources

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -23,6 +23,22 @@ return {
   },
   plugins = {
     {
+      --This will be removed after fix
+      "nvim-neo-tree/neo-tree.nvim",
+      opts = function(_, opts)
+        opts.source_selector = {
+          winbar = true, -- toggle to show selector on winbar
+          statusline = false,
+          sources = {
+            { source = "filesystem" },
+            { source = "buffers" },
+            { source = "git_status" },
+          },
+        }
+        return opts
+      end,
+    },
+    {
       "EdenEast/nightfox.nvim",
     },
     {
