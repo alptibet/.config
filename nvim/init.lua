079bc376874c26514695d4d727200cec2dc33a80
alptibet <a.tibet@yandex.ru>
2023-07-09 23:25:47 +0300

    mini starter

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,6 +6,6 @@ vim.cmd.colorscheme "mellow"
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
   callback = function()
-require("config.autocommands")
+    require("config.autocommands")
   end
 })
--- nvim/lua/plugins/mini/init.lua
+++ nvim/lua/plugins/mini/init.lua
@@ -36,4 +36,36 @@ return {
       require("mini.statusline").setup()
     end
   },
+  {
+    "echasnovski/mini.starter",
+    version = false, -- wait till new 0.7.0 release to put it back on semver
+    event = "VimEnter",
+    opts = function()
+      local logo = table.concat({
+        "     ___           _/7        `  ___  '           '*`           |||           ,,,           +++           '*`      ",
+        "    (o o)         (o o)      -  (O o)  -         (o o)         (o o)         (o o)         (o o)         (o o)     ",
+        "ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo----ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-",
+      }, "\n")
+
+      local config = {
+        header = logo,
+      }
+      return config
+    end,
+    config = function(_, config)
+      -- close Lazy and re-open when starter is ready
+      if vim.o.filetype == "lazy" then
+        vim.cmd.close()
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "MiniStarterOpened",
+          callback = function()
+            require("lazy").show()
+          end,
+        })
+      end
+
+      local starter = require("mini.starter")
+      starter.setup(config)
+    end,
+  }
 }
