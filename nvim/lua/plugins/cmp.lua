02c820eaf67e053e2ea30b2864d31fa09a9d05dd
alptibet <a.tibet@yandex.ru>
2023-09-02 20:43:08 +0300

    remove unnecessary func

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -15,12 +15,6 @@ return {
     local snippets = require("luasnip.loaders.from_vscode")
     local cmp_select_opts = { behavior = cmp.SelectBehavior.Select }
 
-    local hasWordsBefore = function()
-      unpack = unpack or table.unpack
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-
     cmp.setup({
       snippet = {
         expand = function(args)
