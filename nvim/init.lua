aec41496574cecf1cab5f5450cf87452669c35a5
alptibet <a.tibet@yandex.ru>
2023-09-02 22:00:54 +0300

    remove cattpuccin

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,11 @@
 require("config.options")
 require("config.keymaps")
 require("config.lazy")
-vim.cmd.colorscheme "catppuccin-mocha"
+vim.cmd.colorscheme("kanagawa-dragon")
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
   callback = function()
     require("config.autocommands")
-  end
+  end,
 })
