9b747321dbf015fd88195d7a48ec585ef40306f3
alptibet <a.tibet@yandex.ru>
2023-12-18 12:41:50 +0300

    fix luasnip

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -11,6 +11,7 @@ return {
   config = function()
     local cmp = require("cmp")
     local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
     local snippets = require("luasnip.loaders.from_vscode")
     local cmp_select_opts = { behavior = cmp.SelectBehavior.Select }
 
