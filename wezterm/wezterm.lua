5f4d9cefdbde309c1d69dae63e85a47110b08300
alptibet <a.tibet@yandex.ru>
2023-12-27 12:29:02 +0300

    wezterm color scheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,28 +7,28 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 config.font = wezterm.font("Liga SFMono Nerd Font", { weight = "Light" })
-config.color_scheme = "solarized"
+config.color_scheme = "solarized-osaka"
 config.window_background_opacity = 0.8
 config.font_size = 13
 config.line_height = 1.2
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 
 -- config.color_scheme = "nordic"
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
 config.keys = {
-	{ key = "x", mods = "CTRL | ALT", action = wezterm.action.ShowTabNavigator },
+  { key = "x", mods = "CTRL | ALT", action = wezterm.action.ShowTabNavigator },
 }
 -- and finally, return the configuration to wezterm
 return config
