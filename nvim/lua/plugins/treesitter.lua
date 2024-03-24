ccbdfd0c310cf7fe832310df317eb9c6501cda5e
alptibet <a.tibet@yandex.ru>
2024-03-24 21:09:38 +0300

    treesitter textobjects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,31 +1,20 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    { "nvim-treesitter/nvim-treesitter-textobjects" },
+  },
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile" },
   cmd = { "TSUpdateSync" },
   opts = {
-    context_commentstring = {
+    autotag = { enable = true },
+    context_commentstring = { enable = true, enable_autocmd = false },
+    highlight = {
       enable = true,
+      disable = function(_, bufnr) return vim.api.nvim_buf_line_count(bufnr) > 10000 end,
     },
-    highlight = { enable = true },
+    incremental_selection = { enable = true },
     indent = { enable = true },
-    ensure_installed = {
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "tsx",
-      "typescript",
-      "vim",
-      "css",
-      "go"
-    },
-    autotag = {
-      enable = true,
-      enable_rename = true,
-      enable_close = true,
-      enable_close_on_slash = false,
-    },
     textobjects = {
       select = {
         enable = true,
@@ -43,9 +32,46 @@ return {
           ["il"] = { query = "@loop.inner", desc = "inside loop" },
           ["aa"] = { query = "@parameter.outer", desc = "around argument" },
           ["ia"] = { query = "@parameter.inner", desc = "inside argument" },
-        }
-      }
-    }
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true,
+        goto_next_start = {
+          ["]k"] = { query = "@block.outer", desc = "Next block start" },
+          ["]f"] = { query = "@function.outer", desc = "Next function start" },
+          ["]a"] = { query = "@parameter.inner", desc = "Next argument start" },
+        },
+        goto_next_end = {
+          ["]K"] = { query = "@block.outer", desc = "Next block end" },
+          ["]F"] = { query = "@function.outer", desc = "Next function end" },
+          ["]A"] = { query = "@parameter.inner", desc = "Next argument end" },
+        },
+        goto_previous_start = {
+          ["[k"] = { query = "@block.outer", desc = "Previous block start" },
+          ["[f"] = { query = "@function.outer", desc = "Previous function start" },
+          ["[a"] = { query = "@parameter.inner", desc = "Previous argument start" },
+        },
+        goto_previous_end = {
+          ["[K"] = { query = "@block.outer", desc = "Previous block end" },
+          ["[F"] = { query = "@function.outer", desc = "Previous function end" },
+          ["[A"] = { query = "@parameter.inner", desc = "Previous argument end" },
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          [">K"] = { query = "@block.outer", desc = "Swap next block" },
+          [">F"] = { query = "@function.outer", desc = "Swap next function" },
+          [">A"] = { query = "@parameter.inner", desc = "Swap next argument" },
+        },
+        swap_previous = {
+          ["<K"] = { query = "@block.outer", desc = "Swap previous block" },
+          ["<F"] = { query = "@function.outer", desc = "Swap previous function" },
+          ["<A"] = { query = "@parameter.inner", desc = "Swap previous argument" },
+        },
+      },
+    },
   },
   config = function(_, opts)
     require("nvim-treesitter.configs").setup(opts)
