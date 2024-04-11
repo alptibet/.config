beffee8f2481dd70845518d37022a786e4dce665
alptibet <a.tibet@yandex.ru>
2024-04-11 22:34:11 +0300

    tairiki

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,7 @@ require("config.options")
 require("config.keymaps")
 require("config.lazy")
 
-vim.cmd.colorscheme("base16-tomorrow-night")
+vim.cmd.colorscheme("no-clown-fiesta")
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -7,5 +7,14 @@ return {
       })
     end
   },
-  { "RRethy/nvim-base16" }
+  {
+    'deparr/tairiki.nvim',
+    lazy = false,
+    priority = 1000, -- only necessary if you use tairiki as default theme
+    config = function()
+      require('tairiki').setup {
+        transparent = true
+      }
+    end,
+  }
 }
