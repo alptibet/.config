e113f2142450dce577111e45cb3331f99fb5f946
alptibet <a.tibet@yandex.ru>
2024-06-13 21:32:59 +0300

    catppuccin

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -9,4 +9,14 @@ return {
 			})
 		end,
 	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				transparent_background = true,
+			})
+		end,
+	},
 }
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -15,7 +15,7 @@ config.window_background_opacity = 0.95
 config.font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Medium" })
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.color_scheme = "Default Dark (base16)"
-config.font_size = 14
+config.font_size = 16
 config.line_height = 1.2
 config.window_padding = {
 	left = 0,
