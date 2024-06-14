03db0caa696011935a208520648dd779da4f18dd
alptibet <a.tibet@yandex.ru>
2024-06-14 20:16:18 +0300

    mini

--- nvim/lua/plugins/mini/init.lua
+++ nvim/lua/plugins/mini/init.lua
@@ -1,72 +1,72 @@
 return {
-  {
-    "echasnovski/mini.pairs",
-    version = "*",
-    event = "InsertEnter",
-    config = function()
-      require("mini.pairs").setup()
-    end,
-  },
-  {
-    "echasnovski/mini.files",
-    version = false,
-    keys = {
-      {
-        "<leader>e",
-        function()
-          require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+    {
+        "echasnovski/mini.pairs",
+        version = "*",
+        event = "InsertEnter",
+        config = function()
+            require("mini.pairs").setup()
         end,
-        desc = "Mini Files",
-      },
-      {
-        "<leader>E",
-        function()
-          require("mini.files").open(vim.loop.cwd(), true)
-        end,
-        desc = "Mini Files cwd",
-      },
     },
-    config = function()
-      require("mini.files").setup({
-        mappings = {
-          reset = "<TAB>",
+    {
+        "echasnovski/mini.files",
+        version = false,
+        keys = {
+            {
+                "<leader>e",
+                function()
+                    require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+                end,
+                desc = "Mini Files",
+            },
+            {
+                "<leader>E",
+                function()
+                    require("mini.files").open(vim.loop.cwd(), true)
+                end,
+                desc = "Mini Files cwd",
+            },
         },
-      })
-    end,
-  },
-  {
-    "echasnovski/mini.starter",
-    version = false, -- wait till new 0.7.0 release to put it back on semver
-    event = "VimEnter",
-    opts = function()
-      local logo = table.concat({
-        "     ___           _/7        `  ___  '           '*`           |||           ,,,           +++           '*`      ",
-        "    (o o)         (o o)      -  (O o)  -         (o o)         (o o)         (o o)         (o o)         (o o)     ",
-        "ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo----ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-",
-      }, "\n")
-      local starter = require("mini.starter")
-      local config = {
-        header = logo,
-        content_hooks = {
-          starter.gen_hook.aligning("center", "center"),
-        },
-      }
-      return config
-    end,
-    config = function(_, config)
-      -- close Lazy and re-open when starter is ready
-      if vim.o.filetype == "lazy" then
-        vim.cmd.close()
-        vim.api.nvim_create_autocmd("User", {
-          pattern = "MiniStarterOpened",
-          callback = function()
-            require("lazy").show()
-          end,
-        })
-      end
+        config = function()
+            require("mini.files").setup({
+                mappings = {
+                    reset = "<TAB>",
+                },
+            })
+        end,
+    },
+    {
+        "echasnovski/mini.starter",
+        version = false, -- wait till new 0.7.0 release to put it back on semver
+        event = "VimEnter",
+        opts = function()
+            local logo = table.concat({
+                "     ___           _/7        `  ___  '           '*`           |||           ,,,           +++           '*`      ",
+                "    (o o)         (o o)      -  (O o)  -         (o o)         (o o)         (o o)         (o o)         (o o)     ",
+                "ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo----ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-ooO--(_)--Ooo-",
+            }, "\n")
+            local starter = require("mini.starter")
+            local config = {
+                header = logo,
+                content_hooks = {
+                    starter.gen_hook.aligning("center", "center"),
+                },
+            }
+            return config
+        end,
+        config = function(_, config)
+            -- close Lazy and re-open when starter is ready
+            if vim.o.filetype == "lazy" then
+                vim.cmd.close()
+                vim.api.nvim_create_autocmd("User", {
+                    pattern = "MiniStarterOpened",
+                    callback = function()
+                        require("lazy").show()
+                    end,
+                })
+            end
 
-      local starter = require("mini.starter")
-      starter.setup(config)
-    end,
-  },
+            local starter = require("mini.starter")
+            starter.setup(config)
+        end,
+    },
 }
