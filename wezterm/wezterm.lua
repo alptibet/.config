548916f95c528e263f7af1819b30a733a1b8e449
alptibet <a.tibet@yandex.ru>
2024-08-12 13:46:47 +0300

    aerospace

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,7 +12,7 @@ config = {
 	font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Medium" }),
 	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 	color_scheme = "tokyonight_night",
-	font_size = 16,
+	font_size = 14,
 	line_height = 1.05,
 	window_padding = {
 		left = 0,
@@ -25,14 +25,18 @@ config = {
 	enable_tab_bar = false,
 	keys = {
 		{ key = "x", mods = "CTRL | ALT", action = act.ShowTabNavigator },
-		{ key = "p", mods = "CTRL | ALT", action = act.PaneSelect {
-			alphabet = '123456789',
-		}},
+		{
+			key = "p",
+			mods = "CTRL | ALT",
+			action = act.PaneSelect({
+				alphabet = "123456789",
+			}),
+		},
 		{ key = "1", mods = "CTRL | ALT", action = act.ActivatePaneByIndex(0) },
 		{ key = "2", mods = "CTRL | ALT", action = act.ActivatePaneByIndex(1) },
 		{ key = "3", mods = "CTRL | ALT", action = act.ActivatePaneByIndex(2) },
 		{ key = "4", mods = "CTRL | ALT", action = act.ActivatePaneByIndex(3) },
-		{ key = "b", mods = "CTRL | ALT", action = act.CloseCurrentPane {confirm = false} },
+		{ key = "b", mods = "CTRL | ALT", action = act.CloseCurrentPane({ confirm = false }) },
 	},
 }
 return config
